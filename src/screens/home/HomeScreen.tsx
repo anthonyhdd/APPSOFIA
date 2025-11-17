3e4ca2cadbfe44dd9ef8b5327ac4aca4dd941a34
anthonyhdd <haddad.anthony26@gmail.com>
2025-11-17 16:09:40 +0200

    Initial Commit - Full Project

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,1 +1,26 @@
+import React, { useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors } from '../../theme';
 
+export default function HomeScreen({ navigation }) {
+  const insets = useSafeAreaInsets();
+
+  // Navigate to LevelSelection by default
+  useEffect(() => {
+    navigation.navigate('LevelSelection');
+  }, [navigation]);
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      {/* HomeScreen will redirect to LevelSelection */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
